500ad0b4eb61088588e7fa4a5083aa55e8cdd1d3
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-06-03 17:44:44 +0900

    implement array

--- src/main/scala/dog/shebang/Application.scala
+++ src/main/scala/dog/shebang/Application.scala
@@ -11,50 +11,9 @@ object Application {
   def main(args: Array[String]): Unit = {
     val nodeList = Parser.parseAll(Parser.program,
       """
+        | val array = list(1)
         | def main() = {
-        |
-        |   val value = 1
-        |   val result = {
-        |     print(value)
-        |     print(1)
-        |     1000
-        |     1 * 3
-        |   }
-        |
-        |   val equal = value == 1
-        |   print(equal)
-        |
-        |   val notEqual = value != 1
-        |   print(notEqual)
-        |
-        |   val greater = value > 1
-        |   print(greater)
-        |
-        |   val greaterEqual = value >= 1
-        |   print(greaterEqual)
-        |
-        |   val less = value < 1
-        |   print(less)
-        |
-        |   val lessEqual = value <= 1
-        |   print(lessEqual)
-        |
-        |   print(12)
-        |   print(result)
-        |   print(value + 1)
-        |
-        |   val mips = 12 + 3
-        |
-        |   print(mips + value)
-        |   print(plus(1, 3))
-        |   print(if(1) 2 else 3)
-        |   print(if(0) 2 else 3)
-        | }
-        |
-        | def plus(left, right) = {
-        |   val spim = 1
-        |
-        |   print(spim + left + right)
+        |   print(array)
         | }
         |
         |
