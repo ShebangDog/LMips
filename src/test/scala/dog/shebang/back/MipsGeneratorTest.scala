4ef5ac34a513bfe0bddab4c3e402d173cd1947a4
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-06-04 00:00:32 +0900

    test generateExpression for AST.Arithmetic

--- src/test/scala/dog/shebang/back/MipsGeneratorTest.scala
+++ src/test/scala/dog/shebang/back/MipsGeneratorTest.scala
@@ -134,5 +134,35 @@ class MipsGeneratorTest extends AnyFunSpec {
       }
     }
 
+    describe("AST.Arithmetic") {
+      type ASTConstructor = (AST.Expression, AST.Expression) => AST.Arithmetic
+      type IRConstructor = (IR.Mips, IR.Mips) => IR.Arithmetic
+
+      val astIrTupleList = List[(ASTConstructor, IRConstructor)](
+        (AST.Addition, IR.Addition),
+        (AST.Subtraction, IR.Subtraction),
+        (AST.Multiplication, IR.Multiplication),
+        (AST.Division, IR.Division),
+        (AST.Equal, IR.Equal),
+        (AST.NotEqual, IR.NotEqual),
+        //    TODO: GreaterThan用のIRをかく
+        //      (AST.GreaterThan, IR.GreaterThan),
+        (AST.GreaterThanEqual, IR.GreaterThanEqual),
+        (AST.LessThan, IR.LessThan),
+        (AST.LessThanEqual, IR.LessThanEqual),
+      )
+
+      astIrTupleList.foreach { case Tuple2(astConstructor, irConstructor) =>
+        describe(s"when called by $astConstructor") {
+          it(s"should generate $irConstructor") {
+            val ast = astConstructor(AST.Number(1), AST.Number(1))
+            val actual = MipsGenerator.generateArithmetic(ast, EmptyTable)
+            val expect = irConstructor(IR.Number(1), IR.Number(1))
+
+            assert(actual == expect)
+          }
+        }
+      }
+    }
   }
 }
